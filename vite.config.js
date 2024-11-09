ae51f56568333237b023fa172825d0efdbd2f169
nienying <nie-ying23097@hotmail.com>
2024-11-09 23:41:07 +0700

    config for github deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "nienying-portfolio",
+  base: "https://gniy-nein.github.io/nienying-portfolio/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
