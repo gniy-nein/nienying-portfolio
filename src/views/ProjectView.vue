8111f9b5f926d22be497f52326d754f132869a93
nienying <nienying.h@houseofdev.tech>
2024-11-18 14:21:17 +0700

    add image

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -14,11 +14,18 @@
 
 <script setup>
 import ProjectCard from "@/components/ProjectCard.vue";
+import Verscan from "@/assets/images/verscan.png";
+import Sponga from "@/assets/images/sponga.webp";
+import EGAT from "@/assets/images/egat.jpg";
+import Mymirai from "@/assets/images/mymirai.jpg";
+import AssetBlitz from "@/assets/images/techsauce.svg";
+import Hadalabo from "@/assets/images/hada_event.jpg";
+import AI from "@/assets/images/research_ai.png";
 const projects = [
   {
     title: "Verscan",
     description: "Verscan, an asset management system, where I was responsible for front-end development and parts of the back-end. My contributions included implementing new features, creating responsive designs, migrating user data to the latest version, and supporting the mobile application.",
-    image: "https://verscan.sponga.co/img/spongaBanner.c2688f5e.png",
+    image: Verscan,
     tags: [
         {
             name: "Java Script",
@@ -66,7 +73,7 @@ const projects = [
   {
     title: "Sponga",
     description: "Sponga, an E-learning platform where I was responsible for front-end development. My contributions included building responsive designs and researching client-side rendering (CSR) and pre-rendering techniques to optimize SEO",
-    image: "https://www.houseofdev.tech/hubfs/16%20blog%20-%20sponga.png",
+    image: Sponga,
     tags: [
        {
             name: "Java Script",
@@ -86,7 +93,7 @@ const projects = [
   {
     title: "Project with EGAT",
     description: "This project involved developing an appointment management system for the EGAT task. I was responsible for both frontend and backend development, as well as deployment. My contributions included creating responsive designs, implementing new features based on requirements, debugging on both the front and back end, exporting data, handling data migration, and managing deployment.",
-    image: "https://ned.egat.co.th/images/logo/LogoEGATE.jpg",
+    image: EGAT,
     tags: [
         {
             name: "Java Script",
@@ -121,7 +128,7 @@ const projects = [
   {
     title: "Mymirai",
     description: "an chat bot ai platform, where I was responsible for front-end development My contributions included creating responsive designs for Outsource.",
-    image: "https://pbs.twimg.com/media/GT4SurwXUAAgmWk.jpg",
+    image: Mymirai,
     tags: [
         {
             name: "Type Script",
@@ -141,7 +148,7 @@ const projects = [
   {
     title: "Asset Blitz; Spot the Spec",
     description: "I worked on a web game for the HoD X TechSauce Global Summit 2024, where I was responsible for front-end development. My contributions included building responsive designs, establishing WebSocket connections, and implementing the game to integrate seamlessly with the monitor board.",
-    image: "https://www.houseofdev.tech/hubfs/18%20final%202%20edited.svg",
+    image: AssetBlitz,
     tags: [
         {
             name: "Type Script",
@@ -162,7 +169,7 @@ const projects = [
   {
     title: "𝟰𝗘𝗩𝗘 𝘅 𝗛𝗮𝗱𝗮 𝗟𝗮𝗯𝗼 Event",
     description: "I supported the front end development for a web game used at an event, where users tap to pop bubbles and collect scores. My contributions included creating responsive designs and integrating the game with the back-end API.",
-    image: "https://scontent.fkdt3-1.fna.fbcdn.net/v/t39.30808-6/456297769_1026534275542368_6325315736686762672_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=rHM_me5ajuAQ7kNvgFrKDd7&_nc_zt=23&_nc_ht=scontent.fkdt3-1.fna&_nc_gid=AvV-pQepJ3LtEUfwO2eBPh4&oh=00_AYBBu-bPMoZ442YrwI-BqOxw-QLAGD1n5Q8u4U8C1b8rmg&oe=673421DA",
+    image: Hadalabo,
     tags: [
         {
             name: "Java Script",
@@ -182,7 +189,7 @@ const projects = [
   {
     title: "Generative AI Research for Further Development",
     description: "I was responsible for researching and developing a proof of concept (POC) for an AI solution that generates information from uploaded images. This involved crafting effective prompt messages, processing the AI responses, and converting the data for storage in the database.",
-    image: "https://cdn.mos.cms.futurecdn.net/bSdmBqWbHN49gwmHGisNge-1200-80.png",
+    image: AI,
     tags: [
         {
             name: "Java Script",
