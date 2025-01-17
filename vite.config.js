96bd85e646c1b75f64e309fa153c5ef1fb42d50f
nienying <nienying.h@houseofdev.tech>
2025-01-18 03:09:08 +0700

    fix router refresh blank test1

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/nienying-portfolio/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
